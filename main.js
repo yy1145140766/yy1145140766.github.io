8ee17ffc3ebedf7a2d5f889aa6d92cd452a1e809
immortal <1762405771@qq.com>
2022-07-02 17:25:14 +0800

    跳转逻辑

---
+++ main.js
@@ -0,0 +1,23 @@
+window.mainLoad = function () {
+    var search = new URLSearchParams(window.location.search);
+    if (search.has("movecar")) {
+        gotoMoveCar();
+    } else {
+        gotoHomePage();
+    }
+};
+
+window.gotoPage = function (page) {
+    window.location.href = window.location.origin
+        + "/" + page + "index.html"
+        + window.location.search
+        + window.location.hash;
+};
+
+window.gotoHomePage = function () {
+    gotoPage("homepage");
+};
+
+window.gotoMoveCar = function () {
+    gotoPage("movecar");
+}
