091aed4235effa4ae471f64b74d7fa68956b2c91
immortal <1762405771@qq.com>
2022-07-02 17:06:19 +0800

    添加挪车页面和个人主页

---
+++ movecar/main.js
@@ -0,0 +1,20 @@
+window.mainLoad = function () {
+    phoneCall();
+};
+
+window.phoneCall = function () {
+    var phoneNumber = "18328425477";
+    var search = new URLSearchParams(window.location.search);
+    if (search.has("movecar")) {
+        phoneNumber = search.get("movecar");
+    } else {
+        var kv = [];
+        search.forEach(function (value, key) {
+            kv.push({ key: key, value: value });
+        });
+        if (kv.length == 1 && kv[0].value == "") {
+            phoneNumber = kv[0].key;
+        }
+    }
+    window.location.href = "tel://" + phoneNumber;
+};
